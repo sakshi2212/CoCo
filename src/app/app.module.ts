962caf32c7eb8ad514d589a18ca6cb121c9bc5b8
PiyushKatoch <piyushkatoch19@gmail.com>
2020-07-02 00:36:29 +0530

    added basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentsComponent } from './contents/contents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    ContentsComponent
   ],
   imports: [
     BrowserModule,
