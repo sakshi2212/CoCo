f74890cc6f8e75551a3e1fdd66eb8278552f63dd
PiyushKatoch <piyushkatoch19@gmail.com>
2020-07-02 01:48:05 +0530

    routing paths set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ChocolateComponent } from './chocolate/chocolate.component';
+import { CoffeeComponent } from './coffee/coffee.component';
+import { OnlineComponent } from './online/online.component';
+import { NgoComponent } from './ngo/ngo.component';
+import { GiftComponent } from './gift/gift.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { LoginComponent } from './login/login.component';
+import { FbComponent } from './fb/fb.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { InstaComponent } from './insta/insta.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent },
+  {path: '', component: HomeComponent },
+  {path: 'about', component: AboutComponent },
+  {path: 'chocolate', component: ChocolateComponent },
+  {path: 'coffee', component: CoffeeComponent },
+  {path: 'online', component: OnlineComponent },
+  {path: 'ngo', component: NgoComponent },
+  {path: 'login', component: LoginComponent },
+  {path: 'gift', component: GiftComponent },
+  {path: 'jobs', component: JobsComponent },
+  {path: 'contactus', component: ContactusComponent },
+  {path: 'fb', component: FbComponent },
+  {path: 'insta', component: InstaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
-import { ContentsComponent } from './contents/contents.component';
+import { AboutComponent } from './about/about.component';
+import { ChocolateComponent } from './chocolate/chocolate.component';
+import { CoffeeComponent } from './coffee/coffee.component';
+import { OnlineComponent } from './online/online.component';
+import { NgoComponent } from './ngo/ngo.component';
+import { LoginComponent } from './login/login.component';
+import { GiftComponent } from './gift/gift.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { InstaComponent } from './insta/insta.component';
+import { FbComponent } from './fb/fb.component';
+import { SubfooterComponent } from './subfooter/subfooter.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    ContentsComponent
+    AboutComponent,
+    ChocolateComponent,
+    CoffeeComponent,
+    OnlineComponent,
+    NgoComponent,
+    LoginComponent,
+    GiftComponent,
+    JobsComponent,
+    ContactusComponent,
+    InstaComponent,
+    FbComponent,
+    SubfooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
